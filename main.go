f50d8f203a2cbb7d2cb2b7887843e2038d99fc4d
sudachi0114 <jnevariste@gmail.com>
2021-04-03 11:47:02 +0900

    connect to local redis-server by using redigo

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gomodule/redigo/redis"
+)
 
 func main() {
 	fmt.Println("Redigo trial.")
+	c := Connection()
+	defer c.Close()
+}
+
+func Connection() redis.Conn {
+	const Addr = "127.0.0.1:6379"
+
+	c, err := redis.Dial("tcp", Addr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Succesfly Connect to redis @", Addr)
+	return c
 }
