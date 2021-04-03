1d786774b387551a67b3cea2beef6d2d53b6925d
sudachi0114 <jnevariste@gmail.com>
2021-04-03 12:20:07 +0900

    Create function insert (key, value) data into Redis Database

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func main() {
 	c := Connection()
 	defer c.Close()
 
+	// SET key
+	res_set := Set("hoge", "fuga", c)
+	fmt.Println(res_set)
+
 	// GET key
 	res_get := Get("hoge", c)
 	fmt.Println(res_get)
@@ -29,6 +33,14 @@ func Connection() redis.Conn {
 
 func Get(key string, c redis.Conn) string {
 	res, err := redis.String(c.Do("GET", key))
+	if err != nil {
+		panic(err)
+	}
+	return res
+}
+
+func Set(key, value string, c redis.Conn) string {
+	res, err := redis.String(c.Do("SET", key, value))
 	if err != nil {
 		panic(err)
 	}
