7585b91264253d65e8ef832558e40a9c0b388d92
sudachi0114 <jnevariste@gmail.com>
2021-04-04 14:44:02 +0900

    user 登録ができた場合は、users セットに username を追加する

--- infra/createUser.go
+++ infra/createUser.go
@@ -16,9 +16,19 @@ func CreateUser(message string, conn redis.Conn) {
 	if err != nil {
 		panic(err) // redis.String(conn.Do()) にして、何回か同じユーザの登録をリクエストすると、ここでパニックする..??
 	}
+
 	if res == nil {
 		fmt.Printf("User %s has already online.\n", username)
 	} else {
 		fmt.Printf("Create User:[%s] | status:[%s].\n", username, res)
+		AddtoUserGroup(username, conn)
 	}
+}
+
+func AddtoUserGroup(user string, conn redis.Conn) string {
+	res, err := redis.String(conn.Do("SADD", "users", user))
+	if err != nil {
+		panic(err)
+	}
+	return res
 }
