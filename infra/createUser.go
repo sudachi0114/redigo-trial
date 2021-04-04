4ebc6f90c5931e5c6f650e4c0c6dd0a4c66194eb
sudachi0114 <jnevariste@gmail.com>
2021-04-04 14:05:59 +0900

    create user create function

---
+++ infra/createUser.go
@@ -0,0 +1,24 @@
+package infra
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+func CreateUser(message string, conn redis.Conn) {
+	splited := strings.Split(message, " ")
+	username := splited[1]
+
+	userkey := "online." + username
+	res, err := conn.Do("SET", userkey, username, "NX", "EX", "120")
+	if err != nil {
+		panic(err) // redis.String(conn.Do()) にして、何回か同じユーザの登録をリクエストすると、ここでパニックする..??
+	}
+	if res == nil {
+		fmt.Printf("User %s has already online.\n", username)
+	} else {
+		fmt.Printf("Create User:[%s] | status:[%s].\n", username, res)
+	}
+}
