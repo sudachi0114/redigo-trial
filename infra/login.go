ee9508d66aa64aa39b07c3a9460803ebd495c9c3
sudachi0114 <jnevariste@gmail.com>
2021-04-05 15:03:37 +0900

    WILL BE FIX: login 時に TTL 設定をおのあう

--- infra/login.go
+++ infra/login.go
@@ -11,12 +11,22 @@ func Login(message string, conn redis.Conn) (string, string) {
 	splited := strings.Split(message, " ")
 	username := splited[1]
 
-	userkey, err := redis.String(conn.Do("GET", username)) // user が登録されているか?
+	// Login したら GET で登録されているかを確認 => TTL を有効にして上書き
+	//	しようと思ったのだけど、GETSET で TTL は登録できないらしい。
+	//		login が登録とログインを兼ねてしまっていてちょっと気持ち悪い..
+	//		だったら create 1つでいいのでは..??
+	userkey := "online." + username
+	res, err := conn.Do("SET", userkey, username, "NX", "EX", "120")
 	if err != nil {
 		panic(err)
 	}
-	if userkey == "" {
-		fmt.Printf("User %s is not exists.\n", username)
+
+	if res == nil {
+		fmt.Printf("User %s has already online.\n", username)
+	} else {
+		fmt.Printf("Logined User:[%s] | status:[%s].\n", username, res)
+		fmt.Println(username)
 	}
+
 	return username, userkey
 }
