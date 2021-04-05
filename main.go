b0a989cc1933f67dd0ae150047dbdcd89b449a9f
sudachi0114 <jnevariste@gmail.com>
2021-04-05 13:13:00 +0900

    use channel for waiting input

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
+	"time"
 
 	"github.com/sudachi0114/redigo-trial/infra"
 )
@@ -13,25 +11,38 @@ func main() {
 	conn := infra.Connection()
 	defer conn.Close()
 
-	prompt := "(屮`･д･)屮 "
-	bio := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print(prompt)
+	// prompt := "(屮`･д･)屮 "
+	// bio := bufio.NewReader(os.Stdin)
+	// for {
+	// 	fmt.Print(prompt)
 
-		line, _, err := bio.ReadLine()
-		maybeMessage := string(line)
+	// 	line, _, err := bio.ReadLine()
+	// 	maybeMessage := string(line)
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		return
+	// 	}
 
-		if maybeMessage == ".exit" {
-			return
-		} else if strings.Contains(maybeMessage, ".create") {
-			infra.CreateUser(maybeMessage, conn)
-		} else {
-			fmt.Println(maybeMessage)
+	// 	if maybeMessage == ".exit" {
+	// 		return
+	// 	} else if strings.Contains(maybeMessage, ".create") {
+	// 		username, userkey := infra.CreateUser(maybeMessage, conn)
+	// 		fmt.Println("return:", username, userkey)
+	// 	} else {
+	// 		fmt.Println(maybeMessage)
+	// 	}
+	// }
+
+	tickerChan := time.NewTicker(time.Millisecond * 1000).C
+
+	chatExit := false
+
+	for !chatExit {
+		select {
+		case <-tickerChan:
+			fmt.Println(" -- tickerChan -- ")
 		}
 	}
+
 }
